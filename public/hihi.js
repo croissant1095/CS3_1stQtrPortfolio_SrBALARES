50967278d7e8a6980e9a82c8c61bfc25817ae57d
croissant1095 <ryan.reggie.b@gmail.com>
2025-09-02 22:31:57 +0800

    Add hihi.js

--- public/hihi.js
+++ public/hihi.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+console.log("")
+rl = require('readline');
+rl1 = rl.createInterface({
+    input: process.stdin,
+})
+
+rl1.on('line', display)
+
+function display(line) {
+    console.log("You are", line, "years old.")
+    rl1.close()
+}
+
+rl1.on('close', () => {
+    console.log("Try this again!")
+})
